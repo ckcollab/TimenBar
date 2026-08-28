5b223f8fe294ab326030401de1695565663715c6
Eric Carmichael <eric@ckcollab.com>
2026-08-27 20:31:53 -0700

    Release v0.1.6

--- scripts/generate_project.rb
+++ scripts/generate_project.rb
@@ -98,8 +98,8 @@ app_target.build_configurations.each do |config|
   settings["ENABLE_HARDENED_RUNTIME"] = "YES"
   settings["CODE_SIGN_STYLE"] = "Automatic"
   settings["DEVELOPMENT_TEAM"] = "MMJZRMH2BA"
-  settings["CURRENT_PROJECT_VERSION"] = "7"
-  settings["MARKETING_VERSION"] = "0.1.5"
+  settings["CURRENT_PROJECT_VERSION"] = "8"
+  settings["MARKETING_VERSION"] = "0.1.6"
   settings["ASSETCATALOG_COMPILER_APPICON_NAME"] = "AppIcon"
   settings["DEAD_CODE_STRIPPING"] = "YES"
 end
